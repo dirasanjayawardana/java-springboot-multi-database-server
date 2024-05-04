f70b037b7f0eda3d636ac1658e6d6512276ecc24
dirasanjayawardana <dirawardana.dw@gmail.com>
2024-05-04 10:53:43 +0700

    add custom banner and cutom application run

--- src/main/java/com/dira/multidb/MultidbApplication.java
+++ src/main/java/com/dira/multidb/MultidbApplication.java
@@ -1,13 +1,34 @@
 package com.dira.multidb;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class MultidbApplication {
 
+	// run standard spring boot
+	// public static void main(String[] args) {
+	// 	SpringApplication.run(MultidbApplication.class, args);
+	// }
+
+	// run with custom aplication context
 	public static void main(String[] args) {
-		SpringApplication.run(MultidbApplication.class, args);
+		SpringApplication application = new SpringApplication(MultidbApplication.class);
+
+		// add custom settings aplication
+		// contoh mematikan text banner
+		// application.setBannerMode(Banner.Mode.OFF);
+
+		// run application context
+		ConfigurableApplicationContext applicationContext = application.run(args);
+
+		// additional commands
+		// contoh melihat bean (object yang dikelola didalam container spring) "database1DataSourceProperties"
+		DataSourceProperties database1DataSourceProperties = applicationContext.getBean("database1DataSourceProperties", DataSourceProperties.class);
+		System.out.println(database1DataSourceProperties);
 	}
 
 }
